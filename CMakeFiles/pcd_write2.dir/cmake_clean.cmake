c4c853dee5648829a8a212aa6a288b3de9e022cd
donnyllionaire <collinstheprogreammer@gmail.com>
2020-04-02 20:06:05 +0300

    transforming point clouds with a matrix

---
+++ CMakeFiles/pcd_write2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pcd_write2.dir/pcd_write.cpp.o"
+  "pcd_write2.pdb"
+  "pcd_write2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pcd_write2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
